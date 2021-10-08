c0c1ac88c7bd4b4f46fa8a3238f59e93d9beffa2
katydidya92 <sweets1327@gmail.com>
2021-10-08 16:16:24 +0900

    Added like test code

--- src/test/java/shopping/shop/like/service/LikeServiceTest.java
+++ src/test/java/shopping/shop/like/service/LikeServiceTest.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+import shopping.shop.like.domain.Like;
 import shopping.shop.like.repository.LikeRepository;
 import shopping.shop.member.domain.Member;
 import shopping.shop.member.service.MemberService;
 import shopping.shop.post.domain.Post;
 import shopping.shop.post.repository.PostRepository;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,12 +23,26 @@ class LikeServiceTest {
 
     @Autowired LikeService likeService;
     @Autowired MemberService memberService;
+    @Autowired LikeRepository likeRepository;
+    @Autowired PostRepository postRepository;
 
     @Test
     @Transactional
     public void addLike() {
         Member member = memberService.findOne(1L);
         likeService.addLike(member, 3L);
+    }
+
+    @Test
+    public void unLike() {
+        Post post = postRepository.getById(3L);
+        Member member = memberService.findMembers().get(0);
+        Optional<Like> postLike = likeRepository.findByMemberAndPost(member, post);
+        Long likeId = postLike.get().getId();
+
+        if (!likeService.isNotAlreadyLike(member, post)) {
+            likeRepository.deleteById(likeId);
+        }
     }
 
 
