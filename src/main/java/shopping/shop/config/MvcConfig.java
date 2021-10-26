43b7235fd4b31f90e9b4bee86549c6f2f1add710
katydidya92 <sweets1327@gmail.com>
2021-10-26 19:30:39 +0900

    spring security

--- src/main/java/shopping/shop/config/MvcConfig.java
+++ src/main/java/shopping/shop/config/MvcConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class MvcConfig implements WebMvcConfigurer {
-//
-//    // 요청 - 뷰 연결
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/login").setViewName("login/loginForm");
-//        registry.addViewController("/members").setViewName("members/addMemberForm");
-//    }
-//}
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    // 요청 - 뷰 연결
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login/loginForm");
+        registry.addViewController("/members").setViewName("members/addMemberForm");
+    }
+}
--- src/main/java/shopping/shop/config/WebSecurityConfig.java
+++ src/main/java/shopping/shop/config/WebSecurityConfig.java
@@ -11,37 +11,37 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import shopping.shop.member.service.MemberService;
 
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    private final MemberService memberService;
-//
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/css/**", "/images/**", "/js/**", "/*.ico");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/**").permitAll()
-//            .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                .defaultSuccessUrl("/login")
-//                .permitAll()
-//            .and()
-//                .logout()
-//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                .logoutSuccessUrl("/logout")
-//                .invalidateHttpSession(true);
-//
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(memberService).passwordEncoder(new BCryptPasswordEncoder());
-//    }
-//}
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MemberService memberService;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/images/**", "/js/**", "/*.ico");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/login")
+                .permitAll()
+            .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout")
+                .invalidateHttpSession(true);
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
